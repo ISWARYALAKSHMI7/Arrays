e6c68a9355b97a0ef41a1fb436135269c9964977
ISWARYALAKSHMI7 <iswaryalakshmichandran@gmail.com>
2025-05-28 22:46:41 +0530

    using HASHMAP

---
+++ 1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+  def twoSum(self, nums, target):
+    numMap = {}
+    for i, num in enumerate(nums):
+        numMap[num] = i
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in numMap and numMap[complement] != i:
+            return [i, numMap[complement]]
+    return []
