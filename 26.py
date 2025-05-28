b0726c3f2faee0c411495bac3bd341bcab7ac046
ISWARYALAKSHMI7 <iswaryalakshmichandran@gmail.com>
2025-05-28 22:57:36 +0530

    using Two Pointer

---
+++ 26.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n=0
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[n]:
+                n+=1
+                nums[n]=nums[i] 
+        return n+1
